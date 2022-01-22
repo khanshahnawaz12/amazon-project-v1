a10035dabf27d48077949fe9e1a0ddc04139dac3
shahnawaz <khanshahnawaz1947@gmail.com>
2022-01-23 00:29:42 +0530

    init

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCTpferVtQ4nuJuPavNyP1P7bTOhIyti64",
-    authDomain: "clone-3202a.firebaseapp.com",
-    projectId: "clone-3202a",
-    storageBucket: "clone-3202a.appspot.com",
-    messagingSenderId: "543465382281",
-    appId: "1:543465382281:web:1e3b4449b193e57fe5c158"
+    apiKey: "AIzaSyB8acmiORqB3jcLLZzhEXfp7p_aLoNPwJk",
+    authDomain: "project-e1738.firebaseapp.com",
+    projectId: "project-e1738",
+    storageBucket: "project-e1738.appspot.com",
+    messagingSenderId: "995225858547",
+    appId: "1:995225858547:web:dc9ba6698bb005e18fe286"
   };
